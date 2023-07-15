31a36ea4d1f506f20ffe584157f10bc1b3fa465e
Dan <127423122+Coding-Wright@users.noreply.github.com>
2023-07-15 18:34:58 +0100

    Animal Age & Names Arrays

--- Farms/SimpleFarm/MySimpleFarm.cs
+++ Farms/SimpleFarm/MySimpleFarm.cs
@@ -4,14 +4,43 @@ namespace FarmProject.Farms.SimpleFarm
 {
     public class MySimpleFarm : IFarm
     {
+        public int[] AgesOfAnimals = new int[10];
+        public string[] NameOfAnimals = new string[10];
+
+        public int CurrentAnimalIndex = 0;
+
         public void AddAnimal(AnimalType animalType)
         {
-            throw new NotImplementedException();
+            //Generate Age
+            var animalAge = new Random().Next(0, 13);
+
+            AgesOfAnimals[CurrentAnimalIndex] = animalAge;
+
+
+            //Generate Name
+            var animalNameIndex = new Random().Next(0, AnimalNames.Length);
+            var animalname = AnimalNames[animalNameIndex];
+
+            NameOfAnimals[CurrentAnimalIndex] = animalname;
+
+            
+            //Move to next animal
+            CurrentAnimalIndex = CurrentAnimalIndex + 1;
         }
 
         public List<string> GetAnimalDetails()
         {
             throw new NotImplementedException();
         }
+
+
+        public string[] AnimalNames = new string[]
+        {
+            "Name1",
+            "MEW",
+            "Bobby",
+            "Plim",
+            "Plom"
+        };
     }
 }
